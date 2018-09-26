940c1a8755225f53f28780df019acecfbbf9160e
tomoya <33997927+bagaki@users.noreply.github.com>
2018-09-26 15:42:33 +0900

    Add files via upload

---
+++ Full Stack/day57MySQL/1.MySQL简单介绍.py
@@ -0,0 +1,23 @@
+# 单机程序（供用DB）
+
+# MySQL：是用于管理文件的一个软件
+#     服务端软件
+#         socket服务端
+#         本地文件操作
+#         解析指令【SQL语句】
+#     客户端软件（各种各样）
+#         socket客户端
+#         发送指令
+#         解析指令【SQL语句】
+#     PS
+#         DBMS数据库管理系统
+#         SQL语句
+
+# 技能：
+#     安装 服务端和客户端
+#     连接
+#     学习SQL语句规则：指示服务端做任意操作
+
+# 其他类似软件：
+#     关系型数据库：sqllite, db2, oracle, access, sql server MySQL
+#     非关系型数据库：MongoDB, redis
